cfc7cf01aa935765666d8ffc651b4d65302b6c65
KarmaDev <149789206+KarmaDev2023@users.noreply.github.com>
2024-01-20 17:49:13 +0100

    Add files via upload

---
+++ 9000000 - npc_vendor.sql
@@ -0,0 +1,23 @@
+/*
+AzerothCore 2024 - PNJ Vendeur Armes Héritages
+By KarmaDev
+Downloaded from https://github.com/KarmaDev2023/Vendeur-Armes-Heritages
+Bugs and contact with E-mail: karmadev@orange.fr
+*/
+
+DELETE FROM `npc_vendor` WHERE (`entry` = 9000000);																
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES																
+(9000000, 0, 42943, 0, 0, 0, 0),																
+(9000000, 0, 42944, 0, 0, 0, 0),																
+(9000000, 0, 42945, 0, 0, 0, 0),																
+(9000000, 0, 42946, 0, 0, 0, 0),																
+(9000000, 0, 42947, 0, 0, 0, 0),																
+(9000000, 0, 42948, 0, 0, 0, 0),																
+(9000000, 0, 44091, 0, 0, 0, 0),																
+(9000000, 0, 44092, 0, 0, 0, 0),																
+(9000000, 0, 44093, 0, 0, 0, 0),																
+(9000000, 0, 44094, 0, 0, 0, 0),																
+(9000000, 0, 44095, 0, 0, 0, 0),																
+(9000000, 0, 44096, 0, 0, 0, 0),																
+(9000000, 0, 48716, 0, 0, 0, 0),																
+(9000000, 0, 48718, 0, 0, 0, 0);																
